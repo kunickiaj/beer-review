7545add4659843189ebdbd0d6216c47aac79a4cb
Adam Kunicki <adam@wibidata.com>
2015-05-01 22:22:56 -0700

    Add CLI skeleton.

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+CLI for managing your JIRA / Gerrit / git workflow.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='beer-review',
+    version='0.1.0',
+    url='https://github.com/kunickiaj/beer-review',
+    license='BSD',
+    author='Adam Kunicki',
+    author_email='kunickiaj@gmail.com',
+    description='CLI for managing your JIRA / Gerrit / git workflow.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'beer = beer.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
