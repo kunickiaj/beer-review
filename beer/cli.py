61527f86f5871ff46d536e7affbc9320ee978d3e
Adam Kunicki <adam@streamsets.com>
2015-05-05 12:25:45 -0700

    Added initial support for creating new tickets or working on existing ones.

--- beer/cli.py
+++ beer/cli.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
+from beer.process import Brewery
 import click
-from jira.client import JIRA
+import os
+import git
 
-pass_api = click.make_pass_decorator(JIRA)
+pass_brewery = click.make_pass_decorator(Brewery)
+
 
 @click.group()
-@click.option('--user', '-u')
-@click.option('--password', '-p')
-@click.option('--server', '-s')
+@click.option('--config', '-c', default=os.path.expanduser('~/.config/beer-review/beer-review.conf'))
 @click.pass_context
-def main(ctx, user, password, server):
+def main(ctx, config):
     """CLI for managing your JIRA / Gerrit / git workflow."""
-    ctx.obj = JIRA(server=server, basic_auth=(user, password))
+    try:
+        repo = git.Repo(os.path.curdir)
+    except git.InvalidGitRepositoryError as e:
+        click.echo('Current directory is not a git repo!')
+        return -1
+
+    repo
+    ctx.obj = Brewery(config_file_path=config, repo=repo)
+
+
+@main.command('brew')
+@click.argument('issue_id', required=False, default=None)
+@click.option('--issue-type', '-t', default='Bug')
+@click.option('--summary', '-s', default=None)
+@click.option('--description', '-d', default=None)
+@pass_brewery
+def init_jira(ctx, issue_id, issue_type, summary, description):
+    description = summary if description is None else description
+    ctx.work_on(issue_id, issue_type, summary, description)
 
 
 @main.group('project')
@@ -20,8 +39,9 @@ def project():
 
 
 @project.command('list')
-@pass_api
+@pass_brewery
 def project_list(ctx):
     """Lists available projects."""
-    click.echo(ctx.projects())
+    ctx.list_projects()
+
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ CLI for managing your JIRA / Gerrit / git workflow.
 """
 from setuptools import find_packages, setup
 
-dependencies = ['click', 'jira', 'pycrypto']
+dependencies = ['click', 'jira', 'pycrypto', 'ConfigParser', 'gitpython']
 
 setup(
     name='beer-review',
