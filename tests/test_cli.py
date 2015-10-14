76bb00e42720426e29992870244ba8bb44b8720d
Adam Kunicki <adam@streamsets.com>
2015-10-14 10:07:54 -0700

    Update to use user dir for dependencies and add nosetests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,22 +8,3 @@ def runner():
     return CliRunner()
 
 
-def test_cli(runner):
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
-
-
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
-
-
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Adam'])
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, Adam.'
