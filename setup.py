a5d534569e8402aabcedeb32b78f94cec8ef7024
Adam Kunicki <adam@streamsets.com>
2015-11-29 15:56:16 -0800

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = ['click', 'jira', 'pycrypto', 'ConfigParser', 'gitpython']
 
 setup(
     name='beer-review',
-    version='0.1.6',
+    version='0.1.7',
     url='https://github.com/kunickiaj/beer-review',
     license='MIT',
     author='Adam Kunicki',
